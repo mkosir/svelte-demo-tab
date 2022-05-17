c7b7c3e05433ee8ffc8f7d363ab4c746a0ebe9a2
mkosir <kosir.mar@gmail.com>
2022-05-17 16:00:29 +0200

    chore: upgrade Storybook to 6.x

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,23 @@
-import path from "path";
+import path from 'path';
 
-import { StorybookConfig } from "@storybook/core-common";
+import { StorybookConfig } from '@storybook/core-common';
 // import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 
 const storybookConfig: StorybookConfig = {
-  stories: ["../stories/**/*.stories.ts"],
+  stories: ['../stories/**/*.stories.ts'],
   // typescript: { reactDocgen: 'react-docgen' },
   core: {
-    builder: "webpack5",
+    builder: 'webpack5',
   },
   webpackFinal: async (config) => {
     config.module!.rules!.push({
       test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../"),
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
     });
 
     // Resolve absolute imports
-    config.resolve!.modules!.push(path.resolve(process.cwd(), "src"));
+    config.resolve!.modules!.push(path.resolve(process.cwd(), 'src'));
 
     // Plugin 'TsconfigPathsPlugin' issue with Webpack v5 - https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/60
     // config.resolve!.plugins! = [
