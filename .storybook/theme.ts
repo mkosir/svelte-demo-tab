529f140c418757db6aa39e55e02a492ca5a3c0c6
mkosir <kosir.mar@gmail.com>
2022-05-17 14:45:45 +0200

    chore: upgrade Storybook to 6.x

---
+++ .storybook/theme.ts
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export const theme = create({
+  base: 'light',
+  brandTitle: 'Svelte DemoTab 📑',
+  brandUrl: 'https://github.com/mkosir/svelte-demo-tab',
+});
